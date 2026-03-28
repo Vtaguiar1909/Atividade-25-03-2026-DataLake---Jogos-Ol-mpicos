e6840c7add03a6dd3659439b5a53df71ca716f35
Vitor Aguiar <vitoraguiar141@gmail.com>
2026-03-28 20:31:16 -0300

    feat: added folder structures

---
+++ create_metadata.py
@@ -0,0 +1,15 @@
+import json
+from pathlib import Path
+from datetime import date
+
+metadata_schema_json = [
+    {
+        "nome_dataset":"Olympic Games",
+        "descricao":"Base de dados de historicos das olimpiadas de 1986-2022",
+        "caminho":"./raw/Olympic Games.csv",
+        "nivel":"raw",
+        "formato":"CSV",
+        "criado em": "28/03/2026",
+        "versao":"1.0"    
+    }
+]
